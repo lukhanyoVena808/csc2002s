af2f73d8697693c4dc1ec5532c3d456563aa9e16
lookmane001 <lookmane001@gmail.com>
2022-08-05 20:45:02 +0200

    Parallel Programs

---
+++ src/MeanFilter.java
@@ -0,0 +1,7 @@
+
+public class MeanFilter{
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/MedianFilter.java
@@ -0,0 +1,7 @@
+
+public class MedianFilter{
+
+    public static void main(String[] args) {
+        
+    }
+}
